0f90b865a604b69a9a90793dbcff05e4939ab2e8
Angel Fernandez <angelfernandezibanez@gmail.com>
2017-12-08 20:01:16 +0100

    pathlist and shortest path

--- test_oop.py
+++ test_oop.py
@@ -1,5 +1,6 @@
 from math import hypot, sqrt
 from collections import namedtuple
+from operator import itemgetter
 
 import pytest
 
@@ -41,6 +42,22 @@ class Path(object):
         return sum((segment.distance() for segment in self.segments))
 
 
+class PathList(object):
+    __slots__ = ('_paths')
+
+    def __init__(self, paths):
+        self._paths = paths
+
+    @property
+    def paths(self): return self._paths
+
+    def get_shortest_path(self):
+        return min((
+            (path, path.distance())
+            for path in self._paths
+        ), key=itemgetter(1))[0]
+
+
 def test_segment_same_points_error():
     with pytest.raises(ValueError):
         Segment(Point(0, 0), Point(0, 0))
@@ -59,3 +76,13 @@ def test_path_distance():
 
     p2 = Path([])
     assert p2.distance() == 0
+
+
+def test_get_shortest_path():
+    s1 = Segment(Point(0, 0), Point(1, 1))
+    s2 = Segment(Point(1, 1), Point(2, 2))
+    p1 = Path([s1, s2])
+    p2 = Path([s1])
+
+    path_list = PathList([p2, p1])
+    assert path_list.get_shortest_path() == p2
